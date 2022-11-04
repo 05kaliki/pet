9c58c7a28b1ecdae36291ec4522baa35e57aa806
HJ Kang <hkang07@gmail.com>
2022-11-04 18:57:35 +0000

    updated pet test

--- __tests__/newpet.test.js
+++ __tests__/newpet.test.js
@@ -31,7 +31,7 @@ describe('Pet', () => {
     })
 });
 
-describe('growup, walk and feed', () => {
+describe('growUp, walk, feed, checkUp, isAlive, adoptChild and haveBaby', () => {
     let pet;
     let currentAge;
     let currentHunger;
@@ -42,7 +42,6 @@ describe('growup, walk and feed', () => {
  
     beforeEach(() => {
         pet = new Pet('Fido');
-        // const { age: currentAge, hunger: currentHunger, fitness: currentFitness } = pet;
     });
 
     describe('growUp', () => {
@@ -111,6 +110,183 @@ describe('growup, walk and feed', () => {
             expect(() => pet.feed()).toThrow('Your pet is no longer alive :(');
         });
     });
+
+    describe('checkUp', () => {
+        it('if fitness is 3 or less, return "I need a walk"', () => {
+            pet.fitness = 1;
+            pet.hunger = 3;
+            pet.checkUp();
+    
+            expect(pet.checkUp()).toBe('I need a walk');
+        });
+    
+        it('if hunger level is 5 or more, return "I am hungry"', () => {
+            pet.fitness = 5;
+            pet.hunger = 6;
+            pet.checkUp();
+    
+            expect(pet.checkUp()).toBe('I am hungry');
+        });
+    
+        it('if both of the above are true, return "I am hungry AND I need a walk"', () => {
+            pet.fitness = 1;
+            pet.hunger = 6;
+            pet.checkUp();
+    
+            expect(pet.checkUp()).toBe('I am hungry AND I need a walk');
+        });
+    
+        it('when Pet initiated and age is 0, return "Hello! (ᵔᴥᵔ)"', () => {
+            pet.checkUp();
+    
+            expect(pet.checkUp()).toBe('Hello! (ᵔᴥᵔ)');
+        });
+    
+        it('if not hugry, no need to walk and age is not 0, return "I feel great!"', () => {
+            pet.fitness = 5;
+            pet.hunger = 3;
+            pet.age = 1;
+            pet.checkUp();
+    
+            expect(pet.checkUp()).toBe('I feel great!');
+        });
+    
+        it('throws an error if the pet is not alive', () => {
+            pet.age = MAXIMUM_AGE;
+            expect(pet.checkUp()).toBe('Your pet is no longer alive :(');
+        });
+        
+    });
+
+    describe('isAlive', () => {
+        it('if fitness is 0 or less, return false', () => {
+            pet.fitness = -3;
+            pet.hunger = 5;
+            pet.age = 10;
+    
+            expect(pet.isAlive).toBeFalsy();
+        });
+    
+        it('if hunger is 10 or more, return false', () => {
+            pet.fitness = 3;
+            pet.hunger = 12;
+            pet.age = 10;
+    
+            expect(pet.isAlive).toBeFalsy();
+        });
+    
+        it('if age is 30 or more, return false', () => {
+            pet.fitness = 3;
+            pet.hunger = 5;
+            pet.age = 32;
+    
+            expect(pet.isAlive).toBeFalsy();
+        });
+    
+        it('if none of above, return true', () => {
+            pet.fitness = 3;
+            pet.hunger = 5;
+            pet.age = 10;
+    
+            expect(pet.isAlive).toBeTruthy();
+        });
+    })
+
+    describe('adoptChild', () => {
+        const child1 = new Pet('Happy');
+        const child2 = new Pet('Doory');
+
+        it('adopt a child with children property of array', () => {
+            pet.adoptChild(child1);
+
+            expect(pet.children).toEqual([ { name: 'Happy', age: 0, hunger: 0, fitness: 10, children: [] } ]);
+        });
+
+        it('using growUp on first child', () => {
+            child1.growUp();
+
+            expect(child1).toEqual({ name: 'Happy', age: 1, hunger: 5, fitness: 7, children: [] });
+        });
+
+        it('walking child increases its fitness by 4', () => {
+            currentFitness = child1.fitness = 2;
+            child1.walk();
+
+            expect(child1.fitness).toEqual(currentFitness + 4);
+        });
+
+        it('feeding child decreases its hunger by 3', () => {
+            currentHunger = child1.hunger = 7;
+            child1.feed();
+
+            expect(child1.hunger).toEqual(currentHunger - 3);
+        });
+
+        it('adopt two children', () => {
+            pet.adoptChild(child1);
+            pet.adoptChild(child2);
+
+            expect(pet.children).toEqual(
+                [ 
+                    { name: 'Happy', age: 1, hunger: 4, fitness: 6, children: [] },
+                    { name: 'Doory', age: 0, hunger: 0, fitness: 10, children: [] }
+                ]);
+        });
+
+        it('when a dead pet adopts a child, throw an error', () => {
+            pet.age = MAXIMUM_AGE;
+            const child4 = new Pet('Congi');
+            
+            expect(() => pet.adoptChild(child4)).toThrow('Your pet is no longer alive :(');
+
+        });
+    });
+
+    describe('haveBaby', () => { 
+        it('create a baby object inside pet', () => {
+            pet.haveBaby('Happy');
+    
+            expect(pet.children).toEqual([ { name: 'Happy', age: 0, hunger: 0, fitness: 10, children: [] } ]);
+        });
+    
+        it('create two baby objects inside pet', () => {
+            pet.haveBaby('Doory');
+            pet.haveBaby('Nyangi');
+    
+            expect(pet.children).toEqual(
+                [ 
+                    { name: 'Doory', age: 0, hunger: 0, fitness: 10, children: [] },
+                    { name: 'Nyangi', age: 0, hunger: 0, fitness: 10, children: [] } 
+                ]);
+        });
+    
+        it('increase Happy\'s age by 1', () => {
+            pet.haveBaby('Happy');
+            pet.children[0].growUp();
+    
+            expect(pet.children[0]).toEqual({ name: 'Happy', age: 1, hunger: 5, fitness: 7, children: [] });
+        });
+    
+        it('walking child increases its fitness by 4', () => {
+            pet.haveBaby('Happy');
+            currentFitness = pet.children[0].fitness = 2;
+            pet.children[0].walk();
+
+            expect(pet.children[0].fitness).toEqual(currentFitness + 4);
+        });
+
+        it('feeding child decreases its hunger by 3', () => {
+            pet.haveBaby('Happy');
+            currentHunger = pet.children[0].hunger = 7;
+            pet.children[0].feed();
+
+            expect(pet.children[0].hunger).toEqual(currentHunger - 3);
+        });
+    
+        it('when a dead pet adopts a child, throw an error', () => {
+            pet.age = 30;
+            
+            expect(() => pet.haveBaby('Congi')).toThrow('Your pet is no longer alive :(');
+        });
+    });
 })
-
-
